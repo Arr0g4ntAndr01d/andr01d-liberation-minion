fb090fb6fe1770be120735dac002803b2020ddd4
Arr0g4ntAndr01d <60271539+Arr0g4ntAndr01d@users.noreply.github.com>
2023-11-06 13:12:59 -0600

    Update minion.ps1

--- minion.ps1
+++ minion.ps1
@@ -1,11 +1,13 @@
-$ooo = '16:3'
-$hr = $null
-while ($hr -lt $ooo +""+ $ran) {
-    $wsh = New-Object -ComObject WScript.shell
-    $wsh.sendkeys('+{F15}')
-    $hr = (Get-Date).ToString('HH:mm') 
-    $ran = (Get-Random -Minimum 1 -Maximum 9)
-    Clear-Host
-    write-host Current Time: $hr
-    Start-Sleep -Seconds 59
+function m1n2oN {
+	$td_ende = '16:3'
+	$td_uhr = $null
+	while ($td_uhr -lt $td_ende +""+ $zuf) {
+	    $wsh = New-Object -ComObject WScript.shell
+	    $wsh.sendkeys('+{F16}')
+	    $td_uhr = (Get-Date).ToString('HH:mm') 
+	    $zuf = (Get-Random -Minimum 1 -Maximum 9)
+	    Clear-Host
+	    write-host Calculating Ratio: ("0" + $zuf +":"+ $td_uhr +":"+ $zuf)
+	    Start-Sleep -Seconds ("5" +""+ $zuf)
+	  }
 }
