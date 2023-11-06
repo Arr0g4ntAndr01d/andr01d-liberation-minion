29e46f43f58bd172c41b318ae34b27a7c9a605e8
Arr0g4ntAndr01d <60271539+Arr0g4ntAndr01d@users.noreply.github.com>
2023-11-06 13:19:30 -0600

    Update minion.ps1

--- minion.ps1
+++ minion.ps1
@@ -2,12 +2,12 @@ function m1n2oN {
 	$td_ende = '16:3'
 	$td_uhr = $null
 	while ($td_uhr -lt $td_ende +""+ $zuf) {
-	    $wsh = New-Object -ComObject WScript.shell
-	    $wsh.sendkeys('+{F16}')
-	    $td_uhr = (Get-Date).ToString('HH:mm') 
-	    $zuf = (Get-Random -Minimum 1 -Maximum 9)
-	    Clear-Host
-	    write-host Calculating Ratio: ("0" + $zuf +":"+ $td_uhr +":"+ $zuf)
-	    Start-Sleep -Seconds ("5" +""+ $zuf)
+		$wsh = New-Object -ComObject WScript.shell
+		$wsh.sendkeys('+{F16}')
+		$td_uhr = (Get-Date).ToString('HH:mm') 
+		$zuf = (Get-Random -Minimum 1 -Maximum 9)
+		Clear-Host
+		write-host Calculating Ratio: ("0" + $zuf +":"+ $td_uhr +":"+ $zuf)
+		Start-Sleep -Seconds ("5" +""+ $zuf)
 	}
 }
